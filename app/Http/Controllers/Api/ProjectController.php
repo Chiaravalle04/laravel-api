0219e1fd25d1f1b563ad10b47ecfc6dcbdf9e9c2
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-03-29 14:39:26 +0200

    add Api/ProjectController + index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Models
+use App\Models\Project;
+
 class ProjectController extends Controller
 {
     /**
@@ -14,7 +17,14 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Ok',
+            'projects' => $projects
+        ]);
+
     }
 
     /**
